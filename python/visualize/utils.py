b5fe7b7532b4c5d58b1336a6d33a619e78dccd1d
Aldi Piroli <aldipiroli@gmail.com>
2025-06-08 10:14:52 +0200

    added backward ray tracing

--- python/visualize/utils.py
+++ python/visualize/utils.py
@@ -32,7 +32,7 @@ def visualize_camera_poses(camera_poses):
     plt.show()
 
 
-def draw_sphere(center=(0, 0, 0), radius=1, n=20):
+def get_sphere_points(center=(0, 0, 0), radius=1, n=20):
     # https://en.wikipedia.org/wiki/Spherical_coordinate_system
     gamma = np.linspace(0, 2 * np.pi, n)
     theta = np.linspace(0, np.pi, n)
@@ -42,6 +42,10 @@ def draw_sphere(center=(0, 0, 0), radius=1, n=20):
     y = radius * np.sin(theta_m) * np.sin(gamma_m) + center[1]
     z = radius * np.cos(theta_m) + center[2]
     return x, y, z
+
+
+def draw_surface(ax, x, y, z):
+    ax.plot_surface(x, y, z)
 
 
 def get_orthogonal_vectors(v):
@@ -56,7 +60,7 @@ def get_orthogonal_vectors(v):
     return orthogonal
 
 
-def draw_plane(ax, v=(0, 0, 1), p=(0, 0, 0), h=1, w=1):
+def get_plane_points(v=(0, 0, 1), p=(0, 0, 0), h=1, w=1, n=10):
     v = np.array(v, dtype=float)
     p = np.array(p, dtype=float)
     v = v / np.linalg.norm(v)
@@ -65,16 +69,15 @@ def draw_plane(ax, v=(0, 0, 1), p=(0, 0, 0), h=1, w=1):
     v2 = np.cross(v, v1)
     assert np.isclose(np.dot(v2, v), 0)
 
-    i = np.linspace(-h / 2, h / 2, 10)
-    j = np.linspace(-w / 2, w / 2, 10)
+    i = np.linspace(-h / 2, h / 2, n)
+    j = np.linspace(-w / 2, w / 2, n)
     ii, jj = np.meshgrid(i, j)
     points = ii[:, :, None] * v1[None, None, :] + jj[:, :, None] * v2[None, None, :] + p[None, None, :]
 
     x = points[:, :, 0]
     y = points[:, :, 1]
     z = points[:, :, 2]
-    ax.plot_surface(x, y, z, alpha=0.5)
-    return ax
+    return x, y, z
 
 
 def get_unit_vector(start, end):
@@ -99,9 +102,30 @@ def plot_point(ax, p, color="b"):
     return ax
 
 
-def get_vector_from_points(a, b, normalize=True):
+def get_vector_from_points(a, b, normalize=False):
     v = b - a
+    if normalize:
+        norm = np.linalg.norm(v)
+        v /= norm
     return v
+
+
+def draw_parametric_vector(ax, start, v, t, color="g"):
+    end = start + t * v
+    ax.plot([start[0], end[0]], [start[1], end[1]], [start[2], end[2]], color=color, alpha=0.5)
+    ax.plot(end[0], end[1], end[2], marker="D", color="r", alpha=0.5)
+
+
+def get_rays_camera_to_plane(ax, cam, plane):
+    x, y, z = plane
+    pt = np.stack((x, y, z), -1).reshape(-1, 3)  # (n,3) plane points
+
+    for p in pt:
+        v = get_vector_from_points(cam, p)
+        draw_parametric_vector(ax, cam, v, t=3)
+        plot_point(ax, p, color="y")
+
+    return x
 
 
 def vis_camera_poses(data):
@@ -110,15 +134,17 @@ def vis_camera_poses(data):
     ax = fig.add_subplot(111, projection="3d")
 
     # object
-    x, y, z = draw_sphere(center=(0, 0, 0), radius=0.1)
-    ax.plot_surface(x, y, z)
+    x, y, z = get_sphere_points(center=(0, 0, 0), radius=1)
+    draw_surface(ax, x, y, z)
 
     # cameras
     for cam in data:
         ax = plot_point(ax, p=(cam[:3, -1]))  # camera pos
         ax = draw_vector_start_end(ax, start=cam[:3, -1], end=center)  # principal dir
         v = get_unit_vector(start=cam[:3, -1], end=center)
-        ax = draw_plane(ax, v, v)
+        x, y, z = get_plane_points(v=v, p=v)  # image plane
+        # draw_surface(ax, x, y, z)
+        get_rays_camera_to_plane(ax, cam[:3, -1], plane=(x, y, z))
 
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
